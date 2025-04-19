d8981cc2710b07c9bac3c7e586eee5605471ec01
Muhammad Farhad Ajilla <135182880+MFarhadA@users.noreply.github.com>
2025-04-19 10:21:34 +0700

    add block comment

--- level/coin_spawn.gd
+++ level/coin_spawn.gd
@@ -1,26 +1,26 @@
 extends Node2D
 
-@export var coin : PackedScene
-@export var spawn_timer : Timer
-var is_start : bool = false
+@export var coin : PackedScene #scene koin
+@export var spawn_timer : Timer #timer untuk spawn koin
+var is_start : bool = false #boolean untuk memeriksa apakah timer spawn koin dimulai
 
 func _ready() -> void:
-	var coin_spawn = coin.instantiate()
-	var x = randi_range(50, 650)
-	var y = randi_range(50, 400)
-	coin_spawn.position = Vector2(x, y)
-	add_child(coin_spawn)
-	print(coin_spawn.position)
+	#perintah dibawah ini di eksekusi langsung
+	var coin_spawn = coin.instantiate() #instansiasi koin
+	var x = randi_range(50, 650) #pilih acak posisi x dari 50 sampai 650
+	var y = randi_range(50, 400) #pilih acak posisi x dari 40 sampai 400
+	coin_spawn.position = Vector2(x, y) #koin akan disimpan di posisi x dan y
+	add_child(coin_spawn) #tambahkan ke child coin_spawn
 	
 func _process(delta: float) -> void:
-	if not is_start && not get_tree().get_nodes_in_group("coin"):
-		spawn_timer.start()
-		is_start = true
+	if not is_start && not get_tree().get_nodes_in_group("coin"): #jika timer koin belum mulai & tidak ada node group 'koin'
+		spawn_timer.start() #timer spawn koin dimulai
+		is_start = true #boolean timer spawn true
 
 func _on_coin_timer_timeout() -> void:
-	var coin_spawn = coin.instantiate()
-	var x = randi_range(50, 650)
-	var y = randi_range(50, 400)
-	coin_spawn.position = Vector2(x, y)
-	add_child(coin_spawn)
-	is_start = false
+	var coin_spawn = coin.instantiate() #instansiasi koin
+	var x = randi_range(50, 650) #pilih acak posisi x dari 50 sampai 650
+	var y = randi_range(50, 400) #pilih acak posisi x dari 40 sampai 400
+	coin_spawn.position = Vector2(x, y) #koin akan disimpan di posisi x dan y
+	add_child(coin_spawn) #tambahkan ke child coin_spawn
+	is_start = false #boolean timer spawn false
--- player/player.gd
+++ player/player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
-@export var anim : AnimatedSprite2D
-@export var angka : int
-const speed = 300
-var coin : int = 0
+@export var anim : AnimatedSprite2D #animasi player
+const speed = 300 #kecepatan berjalan
+var coin : int = 0 #koin
 
 func _ready() -> void:
 	return
 
 func _physics_process(delta: float) -> void:
 	
-	var velocity = Vector2.ZERO
+	var velocity = Vector2.ZERO #velocity berawal dari posisi nol
 	
+	#movement untuk player
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -21,39 +21,42 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 		
+	#untuk play animasi player
 	if velocity.x != 0 && velocity.y == 0:
 		anim.play("move_right")
-		
 	if velocity.x == 0 && velocity.y != 0:
 		if velocity.y > 0:
 			anim.play("move_down")
 		else:
 			anim.play("move_up")
-	
 	if velocity.x != 0 && velocity.y != 0:
 		if velocity.y > 0:
 			anim.play("move_rightdown")
 		else:
 			anim.play("move_rightup")
-		
+	
+	#flip animasi player jika velocity x nya kurang dari 0
 	if velocity.x > 0:
 		anim.flip_h = false
 	else:
 		anim.flip_h = true
 	
+	#jika diam velocitynya, maka play animasi idle
 	if velocity == Vector2.ZERO:
 		anim.play("idle")
 	
+	#movement player agar bergerak
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		
 	position += velocity * delta
 	
+	#untuk collission player
 	move_and_slide()
 
 
+	#fungsi untuk mendeteksi sesuatu
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("coin"):
-		area.queue_free()
-		coin += 1
+	if area.is_in_group("coin"): #jika nama grup area adalah 'koin'
+		area.queue_free() #area/node tersebut akan dihapus
+		coin += 1 #menambahkan koin
 		
